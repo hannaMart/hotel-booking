0c3ab85fd3c7b7cc479000169f66287429997232
Hanna Martysiuk <annamartysiuk7@gmail.com>
2026-05-09 13:11:19 +0200

    ff

--- hotel-booking-app/src/components/Header.jsx
+++ hotel-booking-app/src/components/Header.jsx
@@ -5,7 +5,7 @@ import InstallPWAButton from "./InstallPWAButton";
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-expand">
+    <nav className="navbar navbar-expand" style="background: black;">
       <div className="container">
         <Link className="navbar-brand" to="/">
           <img className="logo" src={logo} alt="Baltic Breeze Hotel" />
